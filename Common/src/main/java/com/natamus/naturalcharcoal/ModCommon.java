854950d992a7a5f94fea4a1517873836819c56f8
Rick South <rick@natamus.com>
2025-02-18 22:16:22 +0100

    Natural Charcoal source code for 1.21.4.

---
+++ Common/src/main/java/com/natamus/naturalcharcoal/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.naturalcharcoal;
+
+import com.natamus.naturalcharcoal.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
