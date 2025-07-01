6830fa6c6b96920058ffd528cbcc798129ace27e
Rick South <rick@natamus.com>
2025-07-01 10:07:02 +0200

    Natural Charcoal source code for 1.21.7.

---
+++ Common/src/main/java/com/natamus/naturalcharcoal/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.naturalcharcoal.util;
+
+public class Reference {
+	public static final String MOD_ID = "naturalcharcoal";
+	public static final String NAME = "Natural Charcoal";
+	public static final String VERSION = "1.1";
+	public static final String ACCEPTED_VERSIONS = "[1.21.7]";
+}
