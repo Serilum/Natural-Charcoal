2ddef22fde6f007c63fae028740ba93b2b1cd887
Rick South <rick@natamus.com>
2026-09-22 16:27:32 +0200

    (v1.2) (NeoForge) Fixed charcoal not dropping correctly.

--- Common/src/main/java/com/natamus/naturalcharcoal/mixin/FireBlockMixin.java
+++ Common/src/main/java/com/natamus/naturalcharcoal/mixin/FireBlockMixin.java
@@ -3,7 +3,6 @@ package com.natamus.naturalcharcoal.mixin;
 import com.natamus.collective.functions.CompareBlockFunctions;
 import com.natamus.naturalcharcoal.config.ConfigHandler;
 import net.minecraft.core.BlockPos;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -13,24 +12,19 @@ import net.minecraft.world.level.block.FireBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = FireBlock.class, priority = 1001)
 public class FireBlockMixin {
-	@Inject(method = "checkBurnOut(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;ILnet/minecraft/util/RandomSource;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;removeBlock(Lnet/minecraft/core/BlockPos;Z)Z"))
-	private void checkBurnOut(Level level, BlockPos pos, int chance, RandomSource random, int age, CallbackInfo ci) {
+	@Redirect(method = "checkBurnOut", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;removeBlock(Lnet/minecraft/core/BlockPos;Z)Z"))
+	private boolean checkBurnOut_dropCharcoal(Level level, BlockPos pos, boolean isMoving) {
 		BlockState blockState = level.getBlockState(pos);
 		Block block = blockState.getBlock();
 
-		if (!CompareBlockFunctions.isTreeLog(block)) {
-			return;
+		if (CompareBlockFunctions.isTreeLog(block) && Math.random() <= ConfigHandler.burnedLogBecomesCharcoalChance) {
+			level.addFreshEntity(new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, new ItemStack(Items.CHARCOAL, ConfigHandler.charcoalDropAmount)));
 		}
 
-		if (Math.random() > ConfigHandler.burnedLogBecomesCharcoalChance) {
-			return;
-		}
-
-		level.addFreshEntity(new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, new ItemStack(Items.CHARCOAL, ConfigHandler.charcoalDropAmount)));
+		return level.removeBlock(pos, isMoving);
 	}
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 <h2>Natural Charcoal</h2>
-<p><a href="https://github.com/Serilum/Natural-Charcoal"><img src="https://serilum.com/assets/images/logo/natural-charcoal.png"></a></p><h2>Download</h2>
+<p><a href="https://github.com/Serilum/Natural-Charcoal"><img src="https://workflow.serilum.com/web/logo/128/natural-charcoal.png" width="100" height="100"></a></p><h2>Download</h2>
 <p>You can download Natural Charcoal on CurseForge and Modrinth:</p><p>&nbsp;&nbsp;CurseForge: &nbsp;&nbsp;<a href="https://curseforge.com/minecraft/mc-mods/natural-charcoal">https://curseforge.com/minecraft/mc-mods/natural-charcoal</a><br>&nbsp;&nbsp;Modrinth: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://modrinth.com/mod/natural-charcoal">https://modrinth.com/mod/natural-charcoal</a></p>
 <h2>Issue Tracker</h2>
 <p>To keep a better overview of all mods, the issue tracker is located in a separate repository.<br>&nbsp;&nbsp;For issues, ideas, suggestions or anything else, please follow this link:</p>
